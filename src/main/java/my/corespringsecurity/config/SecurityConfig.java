d63453b920daf15fb74cad1ed35a917fc8c7375b
csupreme19 <csupreme19@gmail.com>
2023-06-02 17:55:36 +0900

    기본 보안 설정 - 폼 로그인, static 접근 허용

---
+++ src/main/java/my/corespringsecurity/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package my.corespringsecurity.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(formLogin -> {
+                });
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+}
